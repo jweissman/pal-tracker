909bd2b392f57ba29435bf123327cc25c8126c5f
Joseph Weissman <jweissman@pivotal.io>
2019-02-15 14:39:18 -0500

    passing in mem repo test

--- src/main/java/io/pivotal/pal/tracker/InMemoryTimeEntryRepository.java
+++ src/main/java/io/pivotal/pal/tracker/InMemoryTimeEntryRepository.java
@@ -4,22 +4,67 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class InMemoryTimeEntryRepository {
-    public TimeEntry create(TimeEntry timeEntry) {
-        return new TimeEntry();
+    private List<TimeEntry> entries;
+    private long lastUsedId = 0;
+
+    public InMemoryTimeEntryRepository() {
+        this.entries = new ArrayList<>();
     }
 
-    public TimeEntry find(Long id) {
-        return new TimeEntry();
+    public TimeEntry create(TimeEntry timeEntry) {
+        if (timeEntry.getId() == -1) {
+            Long newId = this.getNextId(); // entries.size() + 1L;
+            timeEntry.setId(newId);
+        }
+        System.out.println(String.format("Adding time entry with ID %d", timeEntry.getId()));
+        this.entries.add(timeEntry);
+
+        return timeEntry;
+
+    }
+
+    public TimeEntry find(long id) {
+        for (TimeEntry entry : this.entries) {
+            if (entry.getId().equals(id)) {
+                System.out.println(
+                        String.format("Found time entry with ID %d", entry.getId())
+                );
+                return entry;
+            }
+        }
+        return null; // null obj?
     }
 
     public List<TimeEntry> list() {
-        return new ArrayList<>();
+        return this.entries; //new ArrayList<>();
     }
 
     public TimeEntry update(Long id, TimeEntry timeEntry) {
-        return new TimeEntry();
+        TimeEntry entryForUpdate = this.find(id);
+        if (entryForUpdate != null) {
+//            entryForUpdate.setUserId(timeEntry.getUserId());
+        }
+        return entryForUpdate; //new TimeEntry();
     }
 
     public void delete(Long id) {
+        List<TimeEntry> entriesToPurge = new ArrayList<>();
+
+        // gather matching entries
+        for (TimeEntry entry : this.entries) {
+            if (entry.getId().equals(id)) {
+                entriesToPurge.add(entry);
+            }
+        }
+
+        // purge!
+        for (TimeEntry entryToPurge : entriesToPurge) {
+            this.entries.remove(entryToPurge);
+        }
+    }
+
+    private Long getNextId() {
+        this.lastUsedId += 1;
+        return this.lastUsedId;
     }
 }
--- src/main/java/io/pivotal/pal/tracker/TimeEntry.java
+++ src/main/java/io/pivotal/pal/tracker/TimeEntry.java
@@ -4,21 +4,63 @@ import java.time.LocalDate;
 
 public class TimeEntry {
     private long id;
+    private long userId;
     private long projectId;
     private LocalDate date;
     private int hours;
 
     public TimeEntry() {
+        this.id = -1;
     }
 
     public TimeEntry(long projectId, long userId, LocalDate date, int hours) {
-//        this.projectId = projectId
+        this.id = -1;
+        this.projectId = projectId;
+        this.userId = userId;
+        this.date = date;
+        this.hours = hours;
     }
 
     public TimeEntry(long timeEntryId, long projectId, long userId, LocalDate date, int hours) {
+        this.id = timeEntryId;
+        this.projectId = projectId;
+        this.userId = userId;
+        this.date = date;
+        this.hours = hours;
     }
 
     public Long getId() {
         return this.id;
     }
+
+    void setId(Long timeEntryId) {
+        this.id = timeEntryId;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj instanceof TimeEntry) {
+            TimeEntry testEntry = (TimeEntry) obj;
+            System.out.println(
+                    String.format(
+                            "Comparing time entries with IDs %s and %s...",
+                            testEntry.getId(),
+                            this.getId()
+                    )
+            );
+            return testEntry.getId().equals(this.getId());
+        } else {
+            return false;
+        }
+    }
+
+    long getUserId() {
+        return this.userId;
+    }
+
+    void setUserId(long newUserId) {
+        this.userId = newUserId;
+    }
+
+
 }
